802f42981a7c94c92c18612c142687c4232ed50a
vinay yadav <vinayyad16@gmail.com>
2024-01-18 00:22:23 +0530

    Added logics to render HTML templates using Node JS

---
+++ index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // OVERVIEW PAGE
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+    const cardsHtml = dataObj.map((card) => replaceTemplate(tempCard, card));
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+  } else if (pathname === '/product') {
+    console.log('QUERY-->', query, pathname);
+    const product = dataObj[query.id || '0'];
+    const output = replaceTemplate(tempProduct, product);
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+    res.end(output);
+  }
+});
+
+server.listen(8080, '127.0.0.1', function () {
+  console.log('SERVER STARTED AT PORT 8080');
+});
